51012dff52def0024066835fa447dd9cadcf99e4
Kristina Faye <kglinoga@gmail.com>
2022-07-18 15:49:20 -0700

    YES got the formatting to be OK! That sucked b/c we had to manually tab/space/delete all that code.. something is wrong w/ my VSC autoformatting, including the lack of wrapping sometimes.  Not sure what the problem is there BUT this app assignment is working as desired (to this point at least)

--- index.js
+++ index.js
@@ -8,93 +8,93 @@ inquirer.prompt([
       message: 'What is the title of your project?',
       name: 'appTitle',
     },
-    
     {
-          type: 'input',
-          message: 'Give the description of your app?',
-          name: 'description',
+      type: 'input',
+      message: 'Give the description of your app?',
+      name: 'description'
+     },
+    {
+      type: 'input',
+      message: 'Enter the Table of Contents for your README:',
+      name: 'tableOfContents',
+     },
+     {
+      type: 'input',
+       message: 'Enter instructions for installing your app:',
+       name: 'installation',
+      },
+     {
+      type: 'input',
+       message: 'Enter instructions for using your app:',
+      name: 'usage',
+      },
+     {
+       type: 'list',
+      message: 'Choose the licenses for your app:',
+      name: 'licenses',
+      choices: ["license1", "license2", "liscense3"]
+      },
+       {
+      type: 'input',
+      message: 'Enter instructions for contributing to your app:',
+       name: 'contributing',
         },
-        {
-              type: 'input',
-              message: 'Enter the Table of Contents for your README:',
-              name: 'tableOfContents',
-            },
-              {
-                  type: 'input',
-                  message: 'Enter instructions for installing your app:',
-                  name: 'installation',
-                },
-                {
-                        type: 'input',
-                        message: 'Enter instructions for using your app:',
-                        name: 'usage',
-                      },
-                      {
-                            type: 'list',
-                            message: 'Choose the licenses for your app:',
-                            name: 'licenses',
-                            choices: ["license1", "license2", "liscense3"]
-                          },
-                          {
-                                type: 'input',
-                                message: 'Enter instructions for contributing to your app:',
-                                name: 'contributing',
-                              },
-                              {
-                                    type: 'input',
-                                    message: 'Enter instructions for testing your app:',
-                                    name: 'testing',
-                                  },
-                                  {
-                                        type: 'input',
-                                        message: 'Enter instructions for asking questions about your app:',
-                                        name: 'questions',
-                                      },
-                                      ])
-                  .then((ans) => 
-                                  !ans.appTitle
-                                    ? console.log('Please enter a Title for your app.')
-                                    : userAnswers(ans)
-                                    // :  appInfo.title = "hello"
-                                );
-                               
-                                const userAnswers = (ans) => {
-                                  console.log(ans);
-                                  const lower = ans.appTitle.toLowerCase();
-                                  fs.writeFile(`./output/${lower}.md`, `# ${ans.appTitle}
+      {
+       type: 'input',
+        message: 'Enter instructions for testing your app:',
+      name: 'testing',
+        },
+      {
+     type: 'input',
+      message: 'Enter instructions for asking questions about your app:',
+      name: 'questions',
+      },
+    ])
 
-                                  ## Description
+    .then((ans) => 
+      !ans.appTitle
+        ? console.log('Please enter a Title for your app.')
+        : userAnswers(ans)
+  );
+                               
+const userAnswers = (ans) => {
+  console.log(ans);
+  const lower = ans.appTitle.toLowerCase();
+  fs.writeFile(`./output/${lower}.md`, 
+  `# ${ans.appTitle}
+
+  ## Description
                                   
-                                  ${ans.description}
+  ${ans.description}
                                   
-                                  ## Table of Contents
+  ## Table of Contents
                                   
-                                  - ${ans.tableOfContents}
+  - ${ans.tableOfContents}
                                   
-                                  ## Installation
+  ## Installation
                                   
-                                  ${ans.installation}
+  ${ans.installation}
                                   
-                                  ## Usage
+  ## Usage
                                   
-                                  ${ans.usage}
+  ${ans.usage}
                                   
-                                  ## Licenses: 
+  ## Licenses: 
                                   
-                                  ${ans.licenses}
+  ${ans.licenses}
                                   
-                                  ## Contributing
+  ## Contributing
                                   
-                                  ${ans.contributing}
+  ${ans.contributing}
                                   
-                                  ## Testing: 
+  ## Testing: 
                                   
-                                  ${ans.testing}
+  ${ans.testing}
                                   
-                                  ## Questions:
+  ## Questions:
                                   
-                                  ${ans.questions}
-                                  `, (err) =>
-                                  err ? console.error(err) : console.log('nice!'))
-                                }
+  ${ans.questions}`,
+    (err) =>
+     err ? console.error(err) : console.log('nice!'))
+}
                                     
---
+++ output/readme maker.md
@@ -0,0 +1,34 @@
+# README Maker
+
+                                  ## Description
+                                  
+                                  it makes readmes for folks via ther Terminal/node.js
+                                  
+                                  ## Table of Contents
+                                  
+                                  - 1. yes 2. no 3. yes again
+                                  
+                                  ## Installation
+                                  
+                                  just use the terminal
+                                  
+                                  ## Usage
+                                  
+                                  see above
+                                  
+                                  ## Licenses: 
+                                  
+                                  license2
+                                  
+                                  ## Contributing
+                                  
+                                  um, just don't pls
+                                  
+                                  ## Testing: 
+                                  
+                                  see above
+                                  
+                                  ## Questions:
+                                  
+                                  see above
+                                  
